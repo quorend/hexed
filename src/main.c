07375e277564c3a7c9a63809061800f4152406ca
Dillon Johnson <dillonjohnson@tutanota.com>
2025-02-22 23:30:30 -0600

    Process command-line arguments

--- src/main.c
+++ src/main.c
@@ -1,8 +1,79 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+#define VERSION "1.0.0"
+
+static void print_version(char *name);
+static void print_help(char *name);
+
+int main(int argc, char *argv[])
 {
-    printf("Lots of work to do...\r\n");
+    if (argc > 1)
+    {
+        /* Process each argument */
+        for (int i = 1; i < argc; i++)
+        {
+            if (argv[i][0] == '-')
+            {
+                /* Options */
+                if (argv[i][1] == '-')
+                {
+                    /* Long option */
+                    if (strcmp(argv[i], "--version") == 0)
+                    {
+                        print_version(argv[0]);
+                    }
+                    else if (strcmp(argv[i], "--help") == 0)
+                    {
+                        print_help(argv[0]);
+                    }
+                }
+                else
+                {
+                    /* Short option */
+                    if (strcmp(argv[i], "-v") == 0)
+                    {
+                        print_version(argv[0]);
+                    }
+                    else if (strcmp(argv[i], "-h") == 0)
+                    {
+                        print_help(argv[0]);
+                    }
+                }
+            }
+            else
+            {
+                /* Filename */
+            }
+        }
+    }
+    else
+    {
+        printf("Lots of work to do...\r\n");
+    }
 
     return 0;
+}
+
+static void print_version(char *name)
+{
+    printf("%s v%s\r\n", strrchr(name, '/') + 1, VERSION);
+
+    return;
+}
+
+static void print_help(char *name)
+{
+    printf(
+        "%s [OPTIONS] [PATH TO FILE]\r\n"
+        "\r\n"
+        "OPTIONS:\r\n"
+        "-h --help\r\n"
+        "           Display this help text.\r\n"
+        "-v --version\r\n"
+        "           Display version information.\r\n",
+        strrchr(name, '/') + 1
+        );
+
+    return;
 }
