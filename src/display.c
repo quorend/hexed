29e994fb2b511badc6a9d785a99058cc2f0c32dd
Dillon Johnson <dillonjohnson@tutanota.com>
2025-04-20 21:47:42 -0500

    Display file contents in both hex and ASCII

--- src/display.c
+++ src/display.c
@@ -23,33 +23,64 @@
  ******************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 
 #include "display.h"
 #include "buffer.h"
 
 #define CLR "\e[1;1H\e[2J"
-#define HEADER "\e[0;33m          " \
+#define HEADER "\e[0;33m            " \
     "0011 2233 4455 6677 8899 aabb ccdd eeff  " \
     "0123456789abcdef\e[0;0m\r\n"
+#define ASCII_STR_LEN 17 /* 16 characters plus NULL character */
 
 void display_draw(void)
 {
+    /* Used for printing the hex byte counts in the left column. */
     size_t addr = buffer_ctx.window.first_row;
-    uint8_t *buff_p = buffer_ctx.buf;
-
-    (void)addr;
-    (void)buff_p;
+    /* A pointer to the buffer that holds the file contents. */
+    uint8_t *buff = buffer_ctx.buf;
+    /* A buffer to hold the current row's ascii representation for printing. */
+    char ascii[ASCII_STR_LEN];
 
     printf("%s", CLR);
     printf("%s", HEADER);
 
-    while (0) /* There are still more bytes to print */
+    while (addr < 0x200) /* TODO: Use terminal height to determine this. */
     {
+        memset(ascii, '\0', ASCII_STR_LEN);
+
         /* Print address */
+        printf("0x%08zX  ", addr);
 
         /* Print bytes */
+        for(uint8_t i = 0; i < 16; i++)
+        {
+            printf("%02X", buff[addr + i]);
+            if((i % 2) == 1)
+            {
+                printf(" ");
+            }
+
+            {
+                char temp[2];
+                if ((buff[addr + i] > 0x1F) && (buff[addr + i] < 0x7F))
+                {
+                    snprintf(temp, 2, "%c", buff[addr + i]);
+                }
+                else
+                {
+                    /* Use period instead of printing special characters. */
+                    snprintf(temp, 2, ".");
+                }
+                strcat(ascii, temp);
+            }
+        }
 
         /* Print ASCII */
+        printf(" %s\r\n", ascii);
+
+        addr += 0x10;
     }
 
     return;
